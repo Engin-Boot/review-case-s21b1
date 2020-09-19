ceec9d74d01b641095d35324e44405e68b7ef359
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-09-19 22:44:27 +0530

    Add files via upload
    Uploaded Sender Tests

---
+++ SenderTests/ConsoleWriterTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sender;
+using Xunit;
+
+namespace SenderTests
+{
+    public class ConsoleWriterTests
+    {
+        public Func<string, string[]> testFunc = line => new string[]{"Passed"};
+        ConsoleWriter writer = new ConsoleWriter();
+
+        [Fact]
+        public void TestGetRowFromLineInvokesPassedFunc()
+        {
+            
+            string[] result = writer.GetRowFromLine("test line", testFunc);
+            string expected = "Passed";
+            Assert.Equal(expected,result[0]);
+        }
+
+        [Fact]
+        public void TestGetSeriesOfWordsInvokesPassedFunc()
+        {
+            string[] result = writer.GetSeriesOfWords("test line", testFunc);
+            string expected = "Passed";
+            Assert.Equal(expected, result[0]);
+        }
+    }
+}
---
+++ SenderTests/StringExtensionsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sender;
+using Xunit;
+
+namespace SenderTests
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void TestStringContainsByIgnoringCase()
+        {
+            string subString = "IamSubString";
+            string mainString = "IambigStringIIncludeiamsubstring";
+            Assert.True(mainString.Contains(subString, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [Fact]
+        public void RaiseExceptionIfSubstringIsNull()
+        {
+            string subString = null;
+            string mainString = "IambigStringIIncludeiamsubstring";
+            Assert.Throws<ArgumentNullException>(() => mainString.Contains(subString, StringComparison.OrdinalIgnoreCase));
+        }
+
+        
+    }
+}
