8fd3acbdf40eee0decdbd0c08a0affefcebe7e6d
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-09-20 17:12:38 +0530

    Update StringExtensions.cs

--- Sender/StringExtensions.cs
+++ Sender/StringExtensions.cs
@@ -14,10 +14,6 @@ namespace Sender
             if (substring == null)
                 throw new ArgumentNullException("substring",
                     "substring cannot be null.");
-            else if (!Enum.IsDefined(typeof(StringComparison), comp))
-                throw new ArgumentException("comp is not a member of StringComparison",
-                    "comp");
-
             return str.IndexOf(substring, comp) >= 0;
         }
     }
