c454371220b37faf6a92106e8193285f22d3fad1
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-09-20 19:23:55 +0530

    Update ConsoleWriterTests.cs

--- SenderTests/ConsoleWriterTests.cs
+++ SenderTests/ConsoleWriterTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +8,24 @@ using Xunit;
 
 namespace SenderTests
 {
+
     public class ConsoleWriterTests
     {
-        public Func<string, string[]> testFunc = line => new string[]{"Passed"};
+        public Func<string, string[]> testFunc = line => new string[] { "Passed" };
         ConsoleWriter writer = new ConsoleWriter();
+
+        [Fact]
+        public void GivenRowDetailsTestForWritableFormat()
+        {
+            string[] rows = new string[]{"4/27/2020 9:14,what does this help,ajay"};
+            string[] actual = writer.GetDataInWritableFormat(rows, 0, 1);
+            string[] expected =
+                {"4/27/2020 9:14 what", "4/27/2020 9:14 does", "4/27/2020 9:14 this", "4/27/2020 9:14 help"};
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.Equal(expected[i],actual[i]);
+            }
+        }
 
         [Fact]
         public void TestGetRowFromLineInvokesPassedFunc()
