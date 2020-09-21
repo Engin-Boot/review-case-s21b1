0d1782a5bd960ca531030855954b54249a324945
univac <68467947+univac-os@users.noreply.github.com>
2020-09-21 17:31:50 +0530

    Update Recieve.cs
    comment remove

--- Reciever/Recieve.cs
+++ Reciever/Recieve.cs
@@ -10,11 +10,11 @@ namespace Reciever
         {
             //we need to take cosole data and send to make file and do the word count
             ConsoleReadAndMakeList read = new ConsoleReadAndMakeList();
-            Writer write = new Writer();
+            WriterCSV write = new Writer(); 
             Dictionary<string, int> wordCount = MakeWordCount.ListToWordCount(read.ReadConsole());
             Console.WriteLine(Directory.GetCurrentDirectory());
             string pathlocation = FileCreater.GetDirectory("file.csv");
-            //Dictionary<string, int> wordCount = new Dictionary<string, int>() { { "wwe", 2 }, { "qqq", 3 }, { "rrr", 1 } };
+            
             write.WriteToCSV(wordCount, pathlocation);
         }
     }
