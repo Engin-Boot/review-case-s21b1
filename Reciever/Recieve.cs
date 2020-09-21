cbeebb33e617f53f91288fb2cc51ae630889187d
univac <68467947+univac-os@users.noreply.github.com>
2020-09-21 18:59:49 +0530

    Update Recieve.cs
    spell error

--- Reciever/Recieve.cs
+++ Reciever/Recieve.cs
@@ -13,9 +13,10 @@ namespace Reciever
             ConsoleReadAndMakeList read = new ConsoleReadAndMakeList();
             WriterCSV write = new Writer(); 
             Dictionary<string, int> wordCount = MakeWordCount.ListToWordCount(read.ReadConsole());
+            string filePath="file.csv"
             write.ValidateOutputFileFormat(filePath);
             Console.WriteLine(Directory.GetCurrentDirectory());
-            string pathlocation = FileCreater.GetDirectory("file.csv");
+            string pathlocation = FileCreater.GetDirectory(filePath);
             
             write.WriteToCSV(wordCount, pathlocation);
         }
